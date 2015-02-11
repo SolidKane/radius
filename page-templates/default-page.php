846ba78119d889f0581faa80d331ad453367d3ca
Demid Surkov <atas.babah@gmail.com>
2015-02-11 22:25:37 +0500

    написаны header footer заведен шаблон page-templates страница о компании

---
+++ page-templates/default-page.php
@@ -0,0 +1,122 @@
+<?php 
+  /* Template Name: Обычная страница */
+  get_template_part('includes/header'); 
+?>
+
+<!-- <div class="container">
+  <div class="row">
+
+    <div class="col-xs-12 col-sm-8">
+      <div id="content" role="main">
+        <?php get_template_part('includes/loops/content', 'page'); ?>
+      </div> --><!-- /#content -->
+<!--     </div>
+    
+    <div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
+      <?php get_template_part('includes/sidebar'); ?>
+    </div>
+    
+  </div> --><!-- /.row -->
+<!-- </div> --><!-- /.container -->
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-2 left-menu">
+      <ul>
+        <li><a href="#">О компании</a></li>
+        <li><a href="#">Клиенты</a></li>
+        <li><a href="#">Новости</a></li>
+        <li><a href="#">Команда</a></li>
+        <li><a href="#">Отзывы</a></li>
+      </ul>
+    </div>
+    <div class="col-sm-10">
+      <h1>Наша компания</h1>
+      <p>Проведем рекламные компании, оформим торговые точки, напечатаем на баннере - быстро и качество! Гарантированно. Команда RADIUS, обладающая многолетним опытом, предлагает Вам следующие</p>
+      <p>Рекламное агентство "Радиус" вышло на рынок рекламы в 2003 году. Компания осуществляет полный цикл работ по производству рекламы. Работа всего коллектива направлена на одну цель - решение задач поставленных заказчиком, и как следствие, выход на долгосрочное сотрудничество. В нашем арсенале мы имеем собственные станки (плоттеры) широкоформатной и интерьерной печати, поэтому можем контролировать весь цикл реализации заказа, и гарантировать качество производимой нами продукции. </p>
+      <!-- видео блок -->
+      <h2>— Собственное производство</h2>
+      <p>Проведем рекламные компании, оформим торговые точки, напечатаем на баннере - быстро и качество! Гарантированно. Команда RADIUS, обладающая многолетним опытом, предлагает Вам следующие</p>
+      <p>Рекламное агентство "Радиус" вышло на рынок рекламы в 2003 цикл реализации заказа, и гарантировать качество производимой нами продукции. </p>
+      <!-- слайдер -->
+      <h2>— Коротко о наших приемуществах</h2>
+      <p>Проведем рекламные компании, оформим торговые точки, напечатаем на баннере - быстро и качество! Гарантированно. Команда RADIUS, обладающая многолетним опытом, предлагает Вам следующие</p>
+      <p>Рекламное агентство "Радиус" вышло на рынок рекламы в 2003 цикл реализации заказа, и гарантировать качество производимой нами продукции. </p>
+      <div class="row">
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/advantages_1.png" alt="">
+          <b>Низкие цены</b>
+          <p>Собственное производство Мы не посредники и работаем по умеренным ценам</p>
+        </div>
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/advantages_2.png" alt="">
+          <b>Гарантии качества</b>
+          <p>Мы даем гарантию на вывески 1 год и в течении этого времени осуществляем бесплатный ремонт</p>
+        </div>
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/advantages_3.png" alt="">
+          <b>Доставка</b>
+          <p>Мы осуществляем доставку рекламной продукции по Челябинску и Челябинской области</p>
+        </div>
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/advantages_4.png" alt="">
+          <b>Бесплатный замер</b>
+          <p>Мы бесплатно приедем к Вам проконсультируем и предложим оптимальный вариант вывески</p>
+        </div>
+      </div>
+      <h2>— С нами надежно</h2>
+      <p>Наша команда специалистов всегда точно и быстро ответит на все ваши вопросы, не зависимо от того готовы ли вы сделать заказ Рекламное агентство "Радиус" вышло на рынок рекламы в 2003 цикл реализации заказа, и гарантировать качество производимой нами продукции. </p>
+      <div class="row">
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/face_1.jpg" alt="">
+          <b>Владимир</b>
+          <p>Специалист по наружной рекламе +7 (351) 778 -85 - 41</p>
+        </div>
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/face_2.jpg" alt="">
+          <b>Александра</b>
+          <p>Специалист широкоформатной печати +7 (351) 778 -85 - 41</p>
+        </div>
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/face_3.jpg" alt="">
+          <b>Евгений</b>
+          <p>Технический директор +7 (351) 778 -85 - 41</p>
+        </div>
+        <div class="col-sm-3 text-center">
+          <img src="<?php echo get_bloginfo('template_directory');?>/img/face_4.jpg" alt="">
+          <b>Оксана</b>
+          <p>Директор +7 (351) 778 -85 - 41</p>
+        </div>
+      </div>
+      <!-- блок "задай вопрос" -->
+      <h2>— Услуги нашей компании</h2>
+      <p>Наша команда специалистов всегда точно и быстро ответит на все ваши вопросы, не зависимо от того готовы ли вы сделать заказ Рекламное агентство "Радиус" вышло на рынок рекламы в 2003 цикл реализации заказа, и гарантировать качество производимой нами продукции. </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2 text-center">
+            <img src="<?php echo get_bloginfo('template_directory');?>/img/services_1.jpg" alt="">
+            <a href="">Наружная реклама</a>
+          </div>
+          <div class="col-sm-2 text-center">
+            <img src="<?php echo get_bloginfo('template_directory');?>/img/services_2.jpg" alt="">
+            <a href="">Печать</a>
+          </div>
+          <div class="col-sm-2 text-center">
+            <img src="<?php echo get_bloginfo('template_directory');?>/img/services_3.jpg" alt="">
+            <a href="">Брендирование транспорта</a>
+          </div>
+          <div class="col-sm-2 text-center">
+            <img src="<?php echo get_bloginfo('template_directory');?>/img/services_4.jpg" alt="">
+            <a href="">Выставочные стенды</a>
+          </div>
+          <div class="col-sm-2 text-center">
+            <img src="<?php echo get_bloginfo('template_directory');?>/img/services_5.jpg" alt="">
+            <a href="">Внутренее оформление</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_template_part('includes/footer'); ?>
